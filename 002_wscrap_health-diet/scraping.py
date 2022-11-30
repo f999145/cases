1c9d0267f544a57b8841d78840106f440aafef7d
f999145 <f999145@yandex.ru>
2022-11-30 23:27:24 +0300

    completed

--- 002_wscrap_health-diet/scraping.py
+++ 002_wscrap_health-diet/scraping.py
@@ -1,32 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import pandas as pd
 import os
 import time
 import random
 from my_decorator import my_decorator
 
-# изменение рабочего каталога
-file_name = os.path.basename(__file__)
-cwd = os.path.abspath(__file__).replace(file_name, '')
-os.chdir(cwd)
+def always_load():
+    # изменение рабочего каталога
+    file_name = os.path.basename(__file__)
+    cwd = os.path.abspath(__file__).replace(file_name, '')
+    os.chdir(cwd)
 
-
-# сохраним индексную страницу
-
-url = 'http://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie'
-host = '/'.join(url.split(sep='/', maxsplit=3)[:3])
-
-headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
-}
+    global url, host, headers
+    url = 'http://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie'
+    host = '/'.join(url.split(sep='/', maxsplit=3)[:3])
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+    }
 
 @my_decorator
 def save_index_html():
+    
     req = requests.get(url=url, headers=headers)
     src = req.text
-    # print(src)
 
     if not os.path.isdir('data'):
         os.mkdir('data')
@@ -36,16 +35,16 @@ def save_index_html():
 
 @my_decorator
 def save_all_categories_dict():
+    
     with open('data/index.html', encoding='utf-8') as file:
         src = file.read()
     
     page = BeautifulSoup(src, 'html.parser')
-    
     all_products_hrefs = page.find_all(class_="mzr-tc-group-item-href")
+    
     all_categories_dict = {}
     
     for item in all_products_hrefs:
-        # print(item)
         item_text = item.text
         item_href = host + item.get('href')
         all_categories_dict[item_text] = item_href
@@ -55,6 +54,7 @@ def save_all_categories_dict():
 
 @my_decorator
 def save_pages_all_category():
+    
     with open('data/all_categories_dict.json', encoding='utf-8') as file:
         all_categories = json.load(file)
     
@@ -62,13 +62,17 @@ def save_pages_all_category():
         os.mkdir('data/cat_folder')
     
     count = 0
+    
     for category_name, category_href in all_categories.items():
         rep =[', ', ' ', ',', '-', '\'']
+        
         for item in rep:
             if item in category_name:
                 category_name = category_name.replace(item, '_')
+                
         req = requests.get(url=category_href, headers=headers)
         src = req.text
+        
         with open(f'data/cat_folder/{count:03d}_{category_name}.html', 'w', encoding='utf-8') as file:
             file.write(src)
             print(f'write \'{count:03d}_{category_name}.html\' completed')
@@ -77,9 +81,83 @@ def save_pages_all_category():
         time.sleep(random.randrange(2,4))
 
 @my_decorator
-def main():
-    print(os.path.isdir('data\cat_folder'))
-    print(os.path.abspath('scraping.py'))
+def create_table():
+    
+    cat_html_list = os.listdir(os.path.join('data', 'cat_folder'))
+    health_diet_base_of_food = pd.DataFrame()
+    
+    products_json_var1 = {}
+    products_json_var2 = []
+    
+    for index, cat_html in enumerate(cat_html_list):
+        
+        with open(
+            os.path.join(os.path.join('data', 'cat_folder', cat_html)),
+            encoding='utf-8'
+        ) as file:
+            src = file.read()
+        
+        category = cat_html[4:-5]
+        page = BeautifulSoup(src, 'html.parser')
+        try:
+            table = page.find(class_='mzr-tc-group-table').find('tbody').find_all('tr')
+            products_json_var1[category] = {}
+            
+            for item in table:
+                product = (item.find_all('td'))
+                tmp = pd.DataFrame({
+                    'Категория': [category],
+                    'Продукт': [product[0].text.strip()],
+                    'Калорийность': [product[1].text.strip()],
+                    'Белки': [product[2].text.strip()],
+                    'Жиры': [product[3].text.strip()],
+                    'Углеводы': [product[4].text.strip()]
+                })
+                
+                products_json_var1[category][product[0].text.strip()] = {
+                    'Калорийность': product[1].text.strip(),
+                    'Белки': product[2].text.strip(),
+                    'Жиры': product[3].text.strip(),
+                    'Углеводы': product[4].text.strip()
+                }
+                
+                products_json_var2.append({
+                    'Категория': category,
+                    'Продукт': product[0].text.strip(),
+                    'Калорийность': product[1].text.strip(),
+                    'Белки': product[2].text.strip(),
+                    'Жиры': product[3].text.strip(),
+                    'Углеводы': product[4].text.strip()
+                })
+                
+                health_diet_base_of_food = pd.concat(
+                                                    [health_diet_base_of_food, tmp],
+                                                    ignore_index=True,
+                                                    sort=False
+                                                )
+
+            print(f'{index:02d} \"{category}\" добавлен')
+
+        except Exception as ex:
+            print(f'{index:02d} {category} ОШИБКА')
+            print('   ', ex)
+    
+    
+    dir = os.path.join('data', 'results')
+    if not os.path.isdir(dir):
+        os.mkdir(dir)
+    
+    with open(os.path.join('data', 'results', 'products_json_var1.json'), 'w', encoding='utf-8') as file:
+        json.dump(products_json_var1, file, indent=4, ensure_ascii=False)
+    
+    with open(os.path.join('data', 'results', 'products_json_var2.json'), 'w', encoding='utf-8') as file:
+        json.dump(products_json_var2, file, indent=4, ensure_ascii=False)
+
+    health_diet_base_of_food.to_csv(os.path.join('data', 'results', 'products.csv'), index=False)    
+
+    print(health_diet_base_of_food.info())
+        
         
 if __name__ == '__main__':
-    main()
+    always_load()
+    create_table()
--- 002_wscrap_health-diet/test.py
+++ 002_wscrap_health-diet/test.py
@@ -1,2 +1,31 @@
-import os
-print(os.getcwd())
+import pandas as pd
+df = pd.DataFrame()
+df1 = pd.DataFrame(
+    {
+        'Courses': ["Spark","PySpark","Python","pandas"],
+        'Fee' : [20000,25000,22000,24000]  
+    }
+)
+df2 = pd.DataFrame(
+    {
+        'Courses': ["Pandas","Hadoop","Hyperion","Java"],
+        'Fee': [25000,25200,24500,24900]  
+    }
+)
+df3 = pd.DataFrame(
+    {
+        'Courses': ["Spark2"],
+        'Fee' : [20000]  
+    }
+)
+df4 = pd.DataFrame(
+    {
+        'Courses': ["Spark4"],
+        'Fee' : [20],
+        'tool': [400]  
+    }
+)
+
+df_01 = pd.concat([df, df1, df2, df3, df4], ignore_index=True, sort=False)
+print(df)
+print(df_01)
